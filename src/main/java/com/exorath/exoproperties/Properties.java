5c452501741bbcf6c3d970dd8a838dc5058b6860
BuildTools <unconfigured@null.spigotmc.org>
2016-08-01 11:55:11 +0630

    Added properties repository.

--- src/main/java/com/exorath/exoproperties/Properties.java
+++ src/main/java/com/exorath/exoproperties/Properties.java
@@ -1,7 +1,41 @@
 package com.exorath.exoproperties;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Toon on 7/31/2016.
  */
 public class Properties {
+    private HashMap<Property, Object> properties = new HashMap();
+
+    public Map<Property, Object> asMap() {
+        return properties;
+    }
+    /**
+     * Assigns a value to a property within this properties repository. This will override any previous value.
+     *
+     * @param property the property to store under
+     * @param value the value you want to store in the key
+     */
+    public void set(Property property, Object value) {
+        properties.put(property, value);
+    }
+
+    public <T> T get(Property<T> property) {
+        if(!properties.containsKey(property))
+            return property.getDefault();
+        return (T) properties.get(property);
+    }
+
+    /**
+     * Returns true if this properties repository has assigned a value (even if it's the same as the default value) for this property, false if not.
+     *
+     * @param property the property
+     *
+     * @return true if this properties repository has a value assigned to the provided property
+     */
+    public boolean contains(Property property) {
+        return properties.containsKey(property);
+    }
 }
--- src/test/com/exorath/exoproperties/PropertiesTest.java
+++ src/test/com/exorath/exoproperties/PropertiesTest.java
@@ -1,7 +1,70 @@
 package com.exorath.exoproperties;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by Toon on 7/31/2016.
  */
 public class PropertiesTest {
+    @Test
+    public void mapEmptyDefaultTest(){
+        Properties properties = new Properties();
+        assertTrue(properties.asMap().isEmpty());
+    }
+    @Test
+    public void setPropertyValueContainsTest(){
+        Properties properties = new Properties();
+        Property<String> property = new Property<>();
+        String value = "random value";
+        properties.set(property, value);
+        assertTrue(properties.asMap().containsKey(property));
+    }
+
+    @Test
+    public void setPropertyValueTest(){
+        Properties properties = new Properties();
+        Property<String> property = new Property<>();
+        String value = "random value";
+        properties.set(property, value);
+        assertEquals(properties.asMap().get(property), value);
+    }
+
+    @Test
+    public void containsPropertyTest(){
+        Properties properties = new Properties();
+        Property<String> property = new Property<>();
+        String value = "blalbalbal";
+        properties.set(property, value);
+        assertTrue(properties.contains(property));
+    }
+
+
+    @Test
+    public void containsPropertyTest2(){
+        Properties properties = new Properties();
+        Property<String> property = new Property<>();
+        assertFalse(properties.contains(property));
+    }
+
+    @Test
+    public void getPropertyTest(){
+        Properties properties = new Properties();
+        Property<String> property = new Property<>();
+        String value = "blalbalbal";
+        properties.set(property, value);
+        assertEquals(properties.get(property), value);
+    }
+
+    @Test
+    public void getPropertyDefaultTest(){
+        Properties properties = new Properties();
+        Property<String> property = new Property<>();
+        String defValue = "This is a default value";
+        property.setDefault(defValue);
+        assertEquals(properties.get(property), defValue);
+    }
 }
